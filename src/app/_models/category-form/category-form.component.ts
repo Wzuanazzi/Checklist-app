f7953e87379ab01765d818ca06fcde47bfafc489
Wladimir Zuanazzi <wzuanazzi@gmail.com>
2024-05-23 14:58:22 -0300

    inclusão dos validators

--- src/app/_models/category-form/category-form.component.ts
+++ src/app/_models/category-form/category-form.component.ts
@@ -3,7 +3,6 @@ import { MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import { MaterialModule } from '../../material.module';
 import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
 import { Category } from '../category';
-import { EventEmitter } from 'stream';
 import { MatError } from '@angular/material/form-field';
 import { MatCommonModule } from '@angular/material/core';
 
@@ -24,17 +23,12 @@ export class CategoryFormComponent implements OnInit{
 
   public categoryForm!: FormGroup;
   public emitForm: boolean = false;
-  //public editedData: any;
-  //public editTableForm !: Category;
-  //public isFormReady = true;
+ 
 
   @Input() public actionName = 'Fixo';
   @Input() public editableCategory!: Category;
 
   
-  positionFormControl = new FormControl('', [Validators.required]);
-  nameFormControl = new FormControl('', [Validators.required]);
-
   constructor(public formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<CategoryFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any ){
@@ -48,12 +42,11 @@ export class CategoryFormComponent implements OnInit{
     this.categoryForm = this.formBuilder.group({
       position: [this.editableCategory != null ? this.editableCategory.position : '', Validators.required],
       name: [this.editableCategory != null ? this.editableCategory.name :'', Validators.required],
-      weight: this.editableCategory != null ? this.editableCategory.weight : '',
+      weight: [this.editableCategory != null ? this.editableCategory.weight : '', Validators.required],
       symbol: this.editableCategory != null ? this.editableCategory.symbol : '',
       });
-
-      //this.isFormReady = true;
-  }
+      console.log("validators ativado");
+    }
 
   
 public clickCancelar () {
